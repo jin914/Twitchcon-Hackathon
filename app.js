97ec8445aaf5fc9abec0bbab4e3f8abee4a543db
Jwl027 <jwl027@ucsd.edu>
2017-10-21 04:04:17 -0700

    Clean up 4AM not much changed except for UI

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('alertButton').addEventListener('click', myAlert);
 });
 */
+
  window.onload = function() {
     var a =0;
     function myFunction()
@@ -45,14 +46,44 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
 
-    function disableForms(){
+    function disableForms(x){
      
-      document.getElementById("atMost").disabled = true;
-      document.getElementById("atLeast").disabled = true;
+     if(x=="d"){
+        document.getElementById('smallStreams').checked = false;
+      }
+      else if(x=="s"){
+        document.getElementById('dontCare').checked = false;
+      }
+      if(document.getElementById('dontCare').checked == false &&document.getElementById('smallStreams').checked == false)
+      {  
+        document.getElementById("at_most").disabled = false;//!(document.getElementById("at_most").disabled);
+        document.getElementById("at_least").disabled = false;//!(document.getElementById("at_least").disabled);
+        
+
+          document.getElementById('searchQuestion').style.opacity = 1;
+        
+      }
+      else {
+
+        document.getElementById("at_most").disabled = true;//!(document.getElementById("at_most").disabled);
+        document.getElementById("at_least").disabled = true;//!(document.getElementById("at_least").disabled);
+        
+
+          document.getElementById('searchQuestion').style.opacity = .5;
+      }
+
+
+      
+      
 
     }
 
-    document.getElementById('dontCare').onclick = disableForms;
+    document.getElementById('dontCare').addEventListener('click',function(){
+      disableForms("d");
+    });
+    document.getElementById('smallStreams').addEventListener('click',function(){
+      disableForms("s");
+    });
 
 
 
@@ -98,6 +129,59 @@ document.addEventListener('DOMContentLoaded', function () {
 
     document.getElementById('user').onclick = page1;
     document.getElementById('userIcon').onclick = page1;
+
+
+
+    function findStream(){
+      if(document.getElementById("dontCare").checked == false && document.getElementById("smallStreams").checked ==false){
+        var at_least = document.getElementById("at_least").value;
+        var at_most = document.getElementById("at_most").value;
+      }
+      else if(document.getElementById("dontCare").checked == true){
+        var at_least = 0;
+        var at_most = 1000000;
+      }
+      else{
+        var at_least = 0;
+        var at_most = 500;
+
+      }
+
+      var at_least = document.getElementById("at_least").value;
+        var at_most = document.getElementById("at_most").value;
+   
+   var total_streams = 10/*total streams ATM*/;
+   var j = 0; //tracks # of streams <=500 viewers
+   var views = [];
+
+   var viewer_count = 0; //TEMPORARY UNTIL WE ACTUALLY GET IT
+   var stream_type ="live"; //Temporary
+   var i;
+
+
+   for( i=0; i<total_streams; i++){
+      if(/*stream #i has a */ viewer_count>=at_least 
+         && viewer_count<=at_most && stream_type=="live"){
+         views.push(id); //stream ID
+         j++;
+      }
+   }
+
+   if(views.size()==0){
+      //error msg: nothing was found with these parameters
+
+      //Create a p that says error!
+   }
+   //now, j = total # of streams w/ at_least<= viewers <=at_most
+   else{
+    var r1 = new Math.random(); //random # from 0 to 1
+   
+    var out = j*r1; // scales it to total # streams <= 500 viewers
+  
+    var out_stream = views[out]; //this is the stream ID to watch
+   }
+   //link to a new page https://go.twitch.tv/[out_stream]
+  }
 }
 
 function showIndex() {
@@ -123,29 +207,7 @@ function showIndex() {
   
   */
   
-function findStream(){
-   var at_least = document.getElementById("at_least").innerHTML;
-   var at_most = docuument.getElementById("at_most").innerHTML;
-   var total_streams = /*total streams ATM*/;
-   var j = 0; //tracks # of streams <=500 viewers
-   var views = [];
-   for(int i=0; i<total_streams; i++){
-      if(/*stream #i has a */ viewer_count>=at_least 
-         && viewer_count<=at_most && stream_type=="live"){
-         views[j] = id; //stream ID
-         j++;
-      }
-   }
-   //now, j = total # of streams w/ at_least<= viewers <=at_most
-   
-   var r1 = new Math.random(); //random # from 0 to 1
-   
-   var out = j*r1; // scales it to total # streams <= 500 viewers
-  
-   var out_stream = views[out]; //this is the stream ID to watch
-   
-   //link to a new page https://go.twitch.tv/[out_stream]
-}
+
 
 
 
